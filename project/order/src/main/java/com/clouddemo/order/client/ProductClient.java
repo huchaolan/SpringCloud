a8175c1f5b971d47655eed6136a44408869abe75
huchaolan@aliyun.com <huchaolan@aliyun.com>
2019-02-15 01:00:02 +0800

    补充商品信息

--- project/order/src/main/java/com/clouddemo/order/client/ProductClient.java
+++ project/order/src/main/java/com/clouddemo/order/client/ProductClient.java
@@ -7,10 +7,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 import com.clouddemo.order.client.bean.ProductInfo;
 
-@FeignClient(name="PRODUCT")
+@FeignClient(name="PRODUCT")//PRODUCT是产品服务注册名称
 public interface ProductClient {
 	
-	@GetMapping("/product/info/byid")
+	@GetMapping("/product/info/byid")//这里要写url全路径不是method方法下的路径
 	List<ProductInfo> getProductInfo(List<String> productIdList);
 	
 	
--- project/order/src/main/java/com/clouddemo/order/service/impl/OrderServiceImpl.java
+++ project/order/src/main/java/com/clouddemo/order/service/impl/OrderServiceImpl.java
@@ -1,15 +1,24 @@
 package com.clouddemo.order.service.impl;
 
 import java.math.BigDecimal;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.clouddemo.order.client.ProductClient;
+import com.clouddemo.order.client.bean.ProductInfo;
+import com.clouddemo.order.dataobject.OrderDetail;
 import com.clouddemo.order.dataobject.OrderMaster;
 import com.clouddemo.order.dto.OrderDTO;
+import com.clouddemo.order.enums.ErrorEnums;
 import com.clouddemo.order.enums.OrderStatusEnum;
 import com.clouddemo.order.enums.PayStatusEnum;
+import com.clouddemo.order.exception.OrderException;
 import com.clouddemo.order.repository.OrderDetailRepository;
 import com.clouddemo.order.repository.OrderMasterRepository;
 import com.clouddemo.order.service.OrderService;
@@ -23,24 +32,79 @@ public class OrderServiceImpl implements OrderService {
 	
 	@Autowired
 	private OrderDetailRepository orderDetailRepository;
-
+	
+	@Autowired
+	private ProductClient prodcutClient;
 	/**
 	 * 创建订单
 	 * @param orderDTO
 	 * @return
 	 */
 	public OrderDTO createOrder(OrderDTO orderDTO) {
-	    //TODO 查询商品信息
+		addProductInfo(orderDTO);
 		//TODO 计算总价
+		countTotalPricel(orderDTO);
 		//TODO 扣库存
+		
 		OrderMaster om = new OrderMaster();
 		orderDTO.setOrderId(IDGenerator.nextId());
 		BeanUtils.copyProperties(orderDTO, om);//复制属性
 		om.setOrderStatus(OrderStatusEnum.NEW.getStatus());
 		om.setPayStatus(PayStatusEnum.UNPAID.getStatus());
-		om.setOrderAmount(new BigDecimal(orderDTO.getOrderDetails().size()));
-		orderMasterRepository.save(om);
+		om = orderMasterRepository.save(om);
+		List<OrderDetail> orderList = orderDTO.getOrderDetails();
+		for(OrderDetail order:orderList) {
+			order.setOrderId(om.getOrderId());
+			order.setDetailId(IDGenerator.nextId());
+		}
+		orderList = orderDetailRepository.saveAll(orderList);
 	    return orderDTO;
+    }
+	
+	/**
+	 * 计算总价
+	 * @param orderDTO
+	 */
+	private void countTotalPricel(OrderDTO orderDTO) {
+		List<OrderDetail> orderList = orderDTO.getOrderDetails();
+		BigDecimal total = new BigDecimal(0);
+		for(OrderDetail od:orderList) {
+			int quan = od.getProductQuantity();
+			BigDecimal price = od.getProductPrice();
+			BigDecimal pp = price.multiply(new BigDecimal(quan));
+			total = total.add(pp);
+		}
+		orderDTO.setOrderAmount(total);
+    }
+
+	/**
+	 * 为订单添加产品信息
+	 * @param orderDTO
+	 */
+	private void addProductInfo(OrderDTO orderDTO) {
+		List<OrderDetail> orderDetailList = orderDTO.getOrderDetails();
+		List<String> productIdList = orderDetailList.stream().map(OrderDetail::getProductId).collect(Collectors.toList());
+		List<ProductInfo> productInfos = prodcutClient.getProductInfo(productIdList);
+		if(productInfos.isEmpty()) {
+			throw new OrderException(ErrorEnums.ERROR_4);
+		}
+		
+		Map<String,ProductInfo> productIdMap = new HashMap<String,ProductInfo>();
+	    for(ProductInfo product:productInfos){
+	    	productIdMap.put(product.getProductId(), product);
+	    }
+	    
+	    for(OrderDetail orderDetail:orderDetailList) {
+	    	String prodcutId=orderDetail.getProductId();
+	    	if(productIdMap.containsKey(prodcutId)) {
+	    		ProductInfo pinfo = productIdMap.get(prodcutId);
+	    		orderDetail.setProductIcon(pinfo.getProductIcon());
+	    		orderDetail.setProductName(pinfo.getProductName());
+	    		orderDetail.setProductPrice(pinfo.getProductPrice());
+	    	}else{
+	    		throw new OrderException(ErrorEnums.ERROR_5);
+	    	}
+	    }
     }
 
 }
