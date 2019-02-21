9eed1612fb15e49cbadbd6725991808d3dab5158
huchaolan@aliyun.com <huchaolan@aliyun.com>
2019-02-21 23:38:05 +0800

    order模块引入productclient模块

--- project/order/src/main/java/com/clouddemo/order/OrderApplication.java
+++ project/order/src/main/java/com/clouddemo/order/OrderApplication.java
@@ -5,9 +5,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
+import com.clouddemo.product.client.ProductClient;
+
 @SpringBootApplication
 @EnableDiscoveryClient
-@EnableFeignClients
+@EnableFeignClients(basePackages="com.clouddemo.product.client")
 public class OrderApplication {
 
 	public static void main(String[] args) {
